f86616cf5e1f391a63aeed6ff5500d9e97f10d14
ehsan44444 <ehsantahafchi@gmail.com>
2025-04-07 20:44:25 +0330

    add BookCreateView

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -12,4 +13,3 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.id])
-
--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('create/', BookCreateView.as_view(), name='book_create'),
+
 
 ]
