9f5a87c398397c76b967b0bf4a2c2c947595f1dd
ehsan44444 <ehsantahafchi@gmail.com>
2025-04-12 13:29:00 +0330

    Development of the comments section

--- books/models.py
+++ books/models.py
@@ -19,9 +19,11 @@ class Book(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.user} : {self.text}'
--- books/views.py
+++ books/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
 
 
 class BookListView(generic.ListView):
@@ -11,9 +13,26 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = 'books/book_detail.html'
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book_comments = book.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            comment_form.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/book_detail.html',
+                  {'book': book, 'comments': book_comments, 'comment_form': comment_form})
 
 
 class BookCreateView(generic.CreateView):
